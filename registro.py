28e127099715d1a6b3c1ca63928bfdb64fbf03fc
FabiolaCury <dufabilitwinski@gmail.com>
2026-06-01 21:59:21 -0300

    Add files via upload

---
+++ registro.py
@@ -0,0 +1,21 @@
+usuarios = [
+    ["Alice", "alice@gmail.com", 30],
+    ["Bob", "bob@gmail.com", 24],
+    ["Carlos", "carlos@gmail.com", 35],
+    ["Diana", "diana@yahoo.com", 28]
+]
+
+# print(usuarios)
+
+primeiro_usuario = usuarios[0]
+print(f"Primeiro usuário: {primeiro_usuario}.")
+
+segundo_usuario = usuarios[1][0]
+print(f"Nome do segundo usuário: {segundo_usuario}.")
+
+terceiro_usuario = usuarios[2]
+print(f"Terceiro usuário: {terceiro_usuario}.")
+
+quarto_usuario = usuarios[3]
+print(f"Quarto usuário: {quarto_usuario}.")
+
